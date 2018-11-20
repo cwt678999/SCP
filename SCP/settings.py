dfc5ee4c0ce81caf9619bd17b2b28e778c5164f7
cwt678999 <cwt678@126.com>
2018-11-20 20:29:03 +0800

    更新设置文件

--- SCP/settings.py
+++ SCP/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    ("css", os.path.join(STATIC_ROOT, 'css')),
+    ("js", os.path.join(STATIC_ROOT, 'js')),
+    ("images", os.path.join(STATIC_ROOT, 'images')),
+
+]
